231df16e11d60944590158dc2d85deaf7d9aed2d
JDRGit <jaimerodriguez85@gmail.com>
2023-10-10 13:06:48 -0400

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import React, { useRef } from 'react';
-import { Form } from 'react-bootstrap';
+import { useEffect, useRef, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import './index.css';
 
 const API_URL = 'https://api.unsplash.com/search/photos';
@@ -8,32 +8,48 @@ const IMAGES_PER_PAGE = 20;
 
 const App = () => {
   const searchInput = useRef(null);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
 
   const fetchImages = async () => {
     try {
       const { data } = await axios.get(
         `${API_URL}?query=${
           searchInput.current.value
-        }&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${
+        }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
           import.meta.env.VITE_API_KEY
         }`
       );
       console.log('data', data);
+      setImages(data.results);
+      setTotalPages(data.total_pages);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const resetSearch = () => {
+    setPage(1);
+    fetchImages();
+  };
+
   const handleSearch = (event) => {
     event.preventDefault();
     console.log(searchInput.current.value);
-    fetchImages();
+    resetSearch();
   };
 
   const handleSelection = (selection) => {
     searchInput.current.value = selection;
-    fetchImages();
+    resetSearch();
   };
+
+  console.log('page', page);
 
   return (
     <div className='container'>
@@ -53,6 +69,24 @@ const App = () => {
         <div onClick={() => handleSelection('birds')}>Birds</div>
         <div onClick={() => handleSelection('dogs')}>Dogs</div>
         <div onClick={() => handleSelection('shoes')}>Shoes</div>
+      </div>
+      <div className='images'>
+        {images.map((image) => (
+          <img
+            key={image.id}
+            src={image.urls.small}
+            alt={image.alt_description}
+            className='image'
+          />
+        ))}
+      </div>
+      <div className='buttons'>
+        {page > 1 && (
+          <Button onClick={() => setPage(page - 1)}>Previous</Button>
+        )}
+        {page < totalPages && (
+          <Button onClick={() => setPage(page + 1)}>Next</Button>
+        )}
       </div>
     </div>
   );
